de381ba6f3527459f69f15233f2c3fff41ffa294
tianzeng <785372699@qq.com>
2017-03-30 17:19:02 +0800

    完成教师信息的数据填充，和分页

--- src/js/components/basedata/pc_staff_data.js
+++ src/js/components/basedata/pc_staff_data.js
@@ -1,9 +1,10 @@
 /**
  * Created by tianzeng on 2017-03-26.
  */
-import {Table, Badge, Menu, Dropdown, Icon} from 'antd';
+import {Pagination ,Table, Badge, Menu, Dropdown, Icon} from 'antd';
 import React from 'react';
 import { connect } from 'dva';
+
 class PCStaffData extends React.Component {
     constructor(){
         super();
@@ -13,17 +14,6 @@ class PCStaffData extends React.Component {
         this.props.dispatch({type: 'users/listAll',payload: ""});
     }
     render() {
-
-        const menu = (
-            <Menu>
-                <Menu.Item>
-                    Action 1
-                </Menu.Item>
-                <Menu.Item>
-                    Action 2
-                </Menu.Item>
-            </Menu>
-        );
         const columns = [
             {title: '姓名', dataIndex: 'teacherName', key: 'teacherName'},
             {title: '工号', dataIndex: 'jobNumber', key: 'jobNumber'},
@@ -31,21 +21,24 @@ class PCStaffData extends React.Component {
             {title: '联系方式', dataIndex: 'contactInformation', key: 'contactInformation'},
             {title: '籍贯', dataIndex: 'nativePlace', key: 'nativePlace'},
         ];
-        const data = [];
-        for (let i = 0; i < 3; ++i) {
-            data.push({
-                key: i,
-                date: '2014-12-24 23:12:00',
-                name: 'This is production name',
-                upgradeNum: 'Upgraded: 56',
-            });
-        }
+        const {list} = this.props.users;
+        const {dispatch} = this.props;
+        const data = list.content;
+        console.log(list.content);
+        var pagination = {
+            total: list.length,
+            defaultCurrent: 1,
+            pageSize: 5,
+            onChange(current) {
+                this.current = current
+                dispatch({
+                    type: 'users/listAll',
+                    payload: {size:5, page:current}
+                });
+            }
+        };
         return (
-            <Table
-                columns={columns}
-                dataSource={data}
-                pagination={false}
-            />
+            <Table rowKey="id" columns={columns} dataSource={data} pagination={pagination} />
         );
     }
 }
